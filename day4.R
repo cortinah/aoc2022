ec1ba7cd23696725081ced157de62de68f7d6302
Hernando Cortina <hch@alum.mit.edu>
2022-12-04 09:58:43 -0500

    edited day4

--- day4.R
+++ day4.R
@@ -4,21 +4,14 @@ library(tidyverse)
 input <- advent_input(4, year = 2022, parse = T)
 
 # part 1
-input |> separate(col = x,into = c('a','b'),sep = ",") -> input
-input |> separate(col = a,into = c('a1','a2'),sep = "-") -> input
-input |> separate(col = b,into = c('b1','b2'),sep = "-") -> input
-input |> mutate_all(as.numeric) -> input
+input |> extract(col=x, into=c("a1","a2","b1","b2"), regex="(\\d+)-(\\d+),(\\d+)-(\\d+)", convert = T) -> input
 
 
-input |> mutate(inc1 = ((a1 >= b1) & (a2 <= b2)) ) -> input
-input |> mutate(inc2 = ((b1 >= a1) & (b2 <= a2)) ) -> input
-input |> mutate(inc = inc1 | inc2) -> input
-
-input |> summarize(ans=sum(inc))
+input |> mutate(inc = ((a1 >= b1) & (a2 <= b2)) | ((b1 >= a1) & (b2 <= a2)) ) -> input
+input |> summarize(ans=sum(inc)) 
 
 # part 2
 
 input |> mutate(inc = ((a1 <= b2) & (a2 >= b1))) -> input
-
 input |> summarize(ans=sum(inc))
 
